3cd9e7999de8af510ee9e6699c51a1c3dc10b23b
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-05 16:26:36 +0300

    lab complete2

--- lib/book.py
+++ lib/book.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-# Book in book.py gets initialized with a title. .                                                                                                                                           [  6%]
-# Book in book.py has the title passed into __init__. .                                                                                                                                      [ 12%]
-# Book in book.py can be assigned an author name. .                                                                                                                                          [ 18%]
-# Book in book.py can be assigned a page count property. .                                                                                                                                   [ 25%]
-# Book in book.py prints "page_count must be an integer" if page_count is not an integer. F                                                                                                  [ 31%]
-# Book in book.py can be assigned a genre. .                                                                                                                                                 [ 37%]
-# Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is called F
 
 class Book:
     def __init__(self, title, page_count=0):
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-# FAILED Shoe in shoe.py gets initialized with a brand. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py has the brand passed to __init__. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py can be assigned a color. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py can be assigned a size. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py prints "size must be an integer" if size is not an integer. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py can be assigned a material. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py can be assigned a condition. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py says that the shoe has been repaired. - TypeError: Shoe() takes no arguments
-# FAILED Shoe in shoe.py has "New" condition after repair. - TypeError: Shoe() takes no arguments
+
 class Shoe:
     def __init__(self, brand, size=0):
         self.brand = brand
-        self.size = 0
+        self.size = size
     
     def cobble(self):
         self.condition = "New"
