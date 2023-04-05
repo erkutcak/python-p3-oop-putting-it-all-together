4ac0a606062fec825c510e0171829bf406ba49ce
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-05 15:58:33 +0300

    lab complete

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+# Book in book.py gets initialized with a title. .                                                                                                                                           [  6%]
+# Book in book.py has the title passed into __init__. .                                                                                                                                      [ 12%]
+# Book in book.py can be assigned an author name. .                                                                                                                                          [ 18%]
+# Book in book.py can be assigned a page count property. .                                                                                                                                   [ 25%]
+# Book in book.py prints "page_count must be an integer" if page_count is not an integer. F                                                                                                  [ 31%]
+# Book in book.py can be assigned a genre. .                                                                                                                                                 [ 37%]
+# Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is called F
 
 class Book:
-    pass
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if type(value) == int:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-
+# FAILED Shoe in shoe.py gets initialized with a brand. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py has the brand passed to __init__. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a color. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a size. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py prints "size must be an integer" if size is not an integer. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a material. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a condition. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py says that the shoe has been repaired. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py has "New" condition after repair. - TypeError: Shoe() takes no arguments
 class Shoe:
-    pass
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self.size = 0
+    
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, value):
+        if type(value) == int:
+            self._size = value
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
